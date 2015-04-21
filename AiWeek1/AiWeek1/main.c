540b8150560212604566ae2cf289c2709e52a756
Guilherme <guilhermealles@users.noreply.github.com>
2015-04-21 12:57:07 +0200

    FIrst Commit

---
+++ AiWeek1/AiWeek1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AiWeek1
+//
+//  Created by Guilherme Alles on 21/04/15.
+//  Copyright (c) 2015 Guilherme Alles. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
