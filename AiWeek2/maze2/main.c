2ac6d754b67b63fb32bc598eaef456f948f90453
Guilherme <guilhermealles@users.noreply.github.com>
2015-05-09 17:27:02 +0200

    .

---
+++ AiWeek2/maze2/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "maze.h"
+#include "state.h"
+#include "prioqueue.h"
+#include "solve.h"
+
+int main(int argc, char *argv[]) {
+  Maze maze;
+  Position pos, goal;
+  
+  readMazeFromFile(&maze, "maze3D.txt");
+
+  getPosition(maze, &pos);
+  printf ("The initial position is ");
+  printPositionWithNewline(pos);
+
+  getGoal(maze, &goal);
+  printf ("The goal is at ");
+  printPositionWithNewline(goal);
+
+  printMaze(maze);
+
+  solveMazeIDA(maze);
+  
+  return EXIT_SUCCESS;
+}
